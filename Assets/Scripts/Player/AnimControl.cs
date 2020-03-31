2790b454cad894651113f7093de82c9b31896567
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-03-31 11:40:26 +0200

    31-3

---
+++ Assets/Scripts/Player/AnimControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour
+{
+    private Animator playerAnim;
+    private Movement playerMov;
+    
+    float cSpeed;
+    //maxSpeed:6f       walkSpeed:1.5f
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerMov = FindObjectOfType<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(playerMov.playerSpeed / 6);
+
+        /*float speed = playerMov.playerSpeed/6;
+        if (speed < 0.05){ cSpeed = 0; }
+        else if (speed<0.55) 
+        {
+            cSpeed = 0.5f;
+        }
+        else if (speed==1)
+        {
+            cSpeed =1;
+        }
+        playerAnim.SetFloat("currentSpeed", cSpeed);*/
+
+
+        axxx();
+        //if (playerMov.maxSpeed == 6) { cSpeed = 2; }
+        // if(playerMov.playerSpeed)
+        //playerAnim.SetFloat("currentSpeed",MaplayerMov.playerSpeed / 6);
+    }
+
+    void axxx()
+    {
+        if ()//averiguar como hacer las anims bien y fluidas
+        {
+
+        }
+    }
+
+    //currentSpeed
+}
