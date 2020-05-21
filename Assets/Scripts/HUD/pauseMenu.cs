424fd09c2ecb3cb1ef55bd64c60607b612d8359b
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-05-21 10:19:54 +0200

    Inicio Menu Pausa
    Solo funciona con raton de momento, el boton de config no hace nada. Son palceholders, hay que hacerlo menos doloroso a la vista

---
+++ Assets/Scripts/HUD/pauseMenu.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class pauseMenu : MonoBehaviour
+{
+    Movement playerSC;
+
+    public GameObject menuPause, buttons, sureDialog;
+
+    //public GameObject buttons;
+
+    bool gamePaused;
+
+    Scene currentScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSC = FindObjectOfType<Movement>();
+        gamePaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScene= SceneManager.GetActiveScene();
+
+        if (Input.GetKeyDown(KeyCode.Escape) && playerSC.playerAlive)
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        menuPause.SetActive(true);
+
+
+        Time.timeScale = 0f;
+        gamePaused = true;        
+
+        /*EventSystem.current.SetSelectedGameObject(ResumeButton);
+        ResumeButton = EventSystem.current.currentSelectedGameObject;*/
+
+    }
+    public void Resume()
+    {
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        menuPause.SetActive(false);
+        //ControlMenuUI.SetActive(false);
+
+
+        Time.timeScale = 1f;
+        gamePaused = false;
+
+    }
+    public void ShowSureMenu() 
+    {
+        buttons.SetActive(false);
+
+        sureDialog.SetActive(true);
+    }
+
+    public void HideSureMenu() 
+    {
+        buttons.SetActive(true);
+
+        sureDialog.SetActive(false);
+
+    }
+
+    public void ExitGame()
+    {
+       Application.Quit();
+    }
+}
