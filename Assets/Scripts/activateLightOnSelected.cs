e2fddc43cda84400b943b9f46c5f825d48b5ea1c
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-05-26 00:37:20 +0200

    nuevo menu inicio
    falta crear y ajustar el menu de confi y la ventana que te pregunta si estas seguro antes de salir

---
+++ Assets/Scripts/activateLightOnSelected.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class activateLightOnSelected : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public EventSystem   eventSystem;
+
+    public GameObject playLight, configLight, exitLight;
+
+    private string selected;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selected = eventSystem.currentSelectedGameObject.gameObject.name;
+
+        if (selected == "Play") 
+        {
+            playLight.SetActive(true);
+            configLight.SetActive(false);
+            exitLight.SetActive(false);
+        } 
+
+        else if (selected == "ConfigBtn")
+        {
+            playLight.SetActive(false);
+            configLight.SetActive(true);
+            exitLight.SetActive(false);
+        } 
+
+        else if (selected == "Exit")
+        {
+            playLight.SetActive(false);
+            configLight.SetActive(false);
+            exitLight.SetActive(true);
+        } 
+
+        print("selected= " + selected);
+    }
+}
