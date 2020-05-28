cd0c2b0ba04fb727d0978bb2d6cf2877c4b2a1cb
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-05-28 13:33:58 +0200

    ajustes de musica y sfx dentro del juego
    Faltan programar cuando un enemigo hataca, muere y esas cosas

---
+++ Assets/Sounds/sfxMIx.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class sfxMIx : MonoBehaviour
+{
+    public AudioMixer sfxMixer;
+    // Start is called before the first frame update
+    public void SetSFXlevel(float vol) 
+    {
+        sfxMixer.SetFloat("sfxVol", vol);
+    }
+}
