0b976fb25e5d198f37dab30d8208ba60d16b99cf
almagro-sergi <seeergi2108@gmail.com>
2020-06-07 19:21:12 +0200

    ooopsy ahora si esta bien el commit

--- Assets/Scripts/EndGameScreen.cs
+++ Assets/Scripts/EndGameScreen.cs
@@ -7,35 +7,18 @@ using UnityEngine.SceneManagement;
 public class EndGameScreen : MonoBehaviour
 {
     public GameObject title, buttons, playAgainBtn, exitBtn, homeBtn;
-    private float timer1, timer2, timer3;
     void Start()
     {
-        title.SetActive(false);
-        buttons.SetActive(false);
+        title.SetActive(true);
+        buttons.SetActive(true);
         EventSystem.current.SetSelectedGameObject(playAgainBtn);
         Cursor.visible = true;
-        timer1 = 10;
-        timer2 = 20;
-        timer3 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer1 <= timer3 && timer3 < 50)
-        {
-            title.SetActive(true);
-        }
 
-        if (timer2 <= timer3 && timer3 < 50)
-        {
-            buttons.SetActive(true);
-        }
-        if (timer3 < 50)
-        {
-            timer3++;
-        }
-        print(timer3);
     }
     public void ExitGame()
     {
