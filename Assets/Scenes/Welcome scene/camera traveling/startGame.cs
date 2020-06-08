70ce52978aadddf1b5d91b4dd728b1d16a674cf3
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-06-08 22:07:19 +0200

    A este comit le llamaré:
    El retraso

--- Assets/Scenes/Welcome scene/camera traveling/startGame.cs
+++ Assets/Scenes/Welcome scene/camera traveling/startGame.cs
@@ -6,7 +6,12 @@ using UnityEngine.SceneManagement;
 public class startGame : MonoBehaviour
 {
     public GameObject load;
-    Scene game = SceneManager.GetSceneByName("Environment");
+    Scene game;
+    private void Start()
+    {
+         game = SceneManager.GetSceneByName("Environment");
+    }
+
     public void StarThetGame()
     {
 
