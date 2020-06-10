f6397362448f846a13fb9ef12bd35075b7b8a777
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-06-10 14:30:16 +0200

    Ahora creo que si es el último commit

---
+++ Assets/Scripts/movingAroundEndgame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class movingAroundEndgame : MonoBehaviour
+{
+    public GameObject playAgainBtn;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void movingAround()
+    {
+        EventSystem.current.SetSelectedGameObject(playAgainBtn);
+    }
+}
---
+++ Assets/Scripts/startGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    public GameObject load;
+    Scene game;
+    private void Start()
+    {
+         game = SceneManager.GetSceneByName("Environment");
+    }
+
+    public void StarThetGame()
+    {
+
+        load.SetActive(true);
+        SceneManager.LoadSceneAsync("Environment");
+
+    }
+    private void Update()
+    {
+        
+        if (game.isLoaded) 
+        {
+            SceneManager.LoadScene("Environment");
+        }
+
+
+    }
+}
